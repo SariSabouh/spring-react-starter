c2d23b381c9b0183b1782dad55b05a57d1e20b1a
SariITG <ssabouh@us.itgsoftware.com>
2019-02-01 14:34:24 -0600

    initial commit

---
+++ src/main/java/com/paliup/rayak/RayakApplication.java
@@ -0,0 +1,14 @@
+package com.paliup.rayak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RayakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RayakApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/paliup/rayak/RayakApplicationTests.java
@@ -0,0 +1,17 @@
+package com.paliup.rayak;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RayakApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
