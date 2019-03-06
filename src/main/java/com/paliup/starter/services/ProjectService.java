346700c6700681de9cfd99856f556f6e7c265ff9
SariITG <ssabouh@us.itgsoftware.com>
2019-03-06 11:36:09 -0600

    finding project by id

--- src/main/java/com/paliup/starter/services/ProjectService.java
+++ src/main/java/com/paliup/starter/services/ProjectService.java
@@ -21,4 +21,12 @@ public class ProjectService {
 			throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
 		}
 	}
+	
+	public Project findProjetByIdentifier(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if (project == null) {
+			throw new ProjectIdException("Project ID '" + projectId.toUpperCase() + "' does not exist");
+		}
+		return  project;
+	}
 }
