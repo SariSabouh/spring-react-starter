519af3803aa280acc7070f85b6c02f2f0244d9eb
SariITG <ssabouh@us.itgsoftware.com>
2019-03-22 14:49:46 -0500

    added backlog and project to repo

---
+++ spring-react-start-backend/src/main/java/com/paliup/starter/repositories/BacklogRepository.java
@@ -0,0 +1,10 @@
+package com.paliup.starter.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.paliup.starter.domain.Backlog;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long>{
+}
