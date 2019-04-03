477ff506a6de00593d0d69b6941bb05e2b57468f
SariITG <ssabouh@us.itgsoftware.com>
2019-04-03 15:11:52 -0500

    setup 401 unauthorized

---
+++ spring-react-start-backend/src/main/java/com/paliup/starter/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.paliup.starter.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private JwtAuthenticationEntryPoint unauthorizedHandler;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable()  // TODO Look into re-enabling this and if we need it
+			.exceptionHandling() // Used to define exception handling
+			.authenticationEntryPoint(unauthorizedHandler) // This handles what is thrown when someone tries to enter not authenticated
+			.and()
+			.sessionManagement() // This line and below means that the server doesn't hold a session The site is stateless
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // TODO Do we need it (Redux should be good enough?)
+			.and()
+			.authorizeRequests() // This and the below allows these endpoints to be accessible without having to be authenticated
+			.antMatchers("/",
+				"/favicon.ico",
+				"/**/*.png",
+				"/**/*.gif",
+				"/**/*.svg",
+				"/**/*.jpg",
+				"/**/*.html",
+				"/**/*.css",
+				"/**/*.js"
+			).permitAll() // TODO this may be something beneficial. Format better when completed and remove if unused
+			.anyRequest().authenticated(); // This means that any other request besdies the permitted ones above need to be authenticated
+	}
+
+}
---
+++ spring-react-start-backend/src/main/java/com/paliup/starter/web/UserController.java
@@ -0,0 +1,5 @@
+package com.paliup.starter.web;
+
+public class UserController {
+
+}
