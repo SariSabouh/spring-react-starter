30333837b01ccc3e3e69d4708151b11639eb0a2f
SariITG <ssabouh@us.itgsoftware.com>
2019-05-02 16:45:29 -0500

    updated build script and moevd to BE resources dynamically

---
+++ spring-react-start-backend/src/main/resources/static/precache-manifest.2288e2756d3325091c037a448b607b0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "43024ae382361e63d93c",
+    "url": "/static/js/main.43024ae3.chunk.js"
+  },
+  {
+    "revision": "7b16046dffb894206f53",
+    "url": "/static/js/2.7b16046d.chunk.js"
+  },
+  {
+    "revision": "43024ae382361e63d93c",
+    "url": "/static/css/main.8cb5a267.chunk.css"
+  },
+  {
+    "revision": "7b16046dffb894206f53",
+    "url": "/static/css/2.f5164af2.chunk.css"
+  },
+  {
+    "revision": "6508198bfc8f679c2867c59d3eb646e2",
+    "url": "/index.html"
+  }
+];
--- spring-react-start-client/postbuild.bat
+++ spring-react-start-client/postbuild.bat
@@ -1,1 +1,8 @@
-move build ../spring-react-start-backend
+set buildPath="..\spring-react-start-backend\src\main\resources\static"
+
+ECHO "Attempting to delete old bundle"
+rmdir /s /q %buildPath%
+ECHO "Deleted old bundle"
+ECHO "Attempting to move bundle to backend static resources"
+move build %buildPath%
+ECHO "Move successful! Build is ready for deployment"
