9d1b14dc94548ddba53b6203dba4031be4f2c913
SariITG <ssabouh@us.itgsoftware.com>
2019-05-03 15:23:39 -0500

    fixed more routing issues

--- spring-react-start-backend/src/main/java/com/paliup/starter/security/WebConfiguration.java
+++ spring-react-start-backend/src/main/java/com/paliup/starter/security/WebConfiguration.java
@@ -1,15 +1,27 @@
 package com.paliup.starter.security;
 
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-  @Override
-  public void addViewControllers(ViewControllerRegistry registry) {
-      registry.addViewController("/{spring:\\w+|\\w+.html}")
-            .setViewName("forward:/");
-  }
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/{spring:(?!api\\b)\\b\\w+}").setViewName("forward:/");
+		registry.addViewController("/notFound").setViewName("forward:/");
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+		return container -> {
+			container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notFound"));
+		};
+	}
 }
