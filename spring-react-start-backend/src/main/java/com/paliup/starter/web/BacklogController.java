0f2aef39a5549e325b50d57f32892f366da73910
SariITG <ssabouh@us.itgsoftware.com>
2019-05-10 09:59:29 -0500

    remove crossorigin

--- spring-react-start-backend/src/main/java/com/paliup/starter/web/BacklogController.java
+++ spring-react-start-backend/src/main/java/com/paliup/starter/web/BacklogController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,7 +21,6 @@ import com.paliup.starter.services.ProjectTaskService;
 
 @RestController
 @RequestMapping("/api/backlog")
-@CrossOrigin // TODO: Remove all CrossOrigins
 public class BacklogController {
 
 	@Autowired	
--- spring-react-start-backend/src/main/java/com/paliup/starter/web/UserController.java
+++ spring-react-start-backend/src/main/java/com/paliup/starter/web/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +27,6 @@ import com.paliup.starter.validator.UserValidator;
 
 @RestController
 @RequestMapping("api/users")
-@CrossOrigin
 public class UserController {
 	
 	@Autowired
